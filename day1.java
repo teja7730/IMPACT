2e97fdee81ea55c696d0ad4f48badc83c871241b
tejesh <tejahyderhan@gmail.com>
2025-05-16 10:08:33 +0530

    intial commit

--- day1.java
+++ day1.java
@@ -1,1 +1,99 @@
- 
+ import java.util.Scanner;
+// class Main {
+//     public static void main(String[] args) {
+//         int age = 18;
+//         if(age>=18){
+//         System.out.println("eligible for vote");}
+//         else{
+//         System.out.println("not eligible");}
+//     }
+// }
+//-----------------------------------------------------------
+
+// class Main{
+//     public static void main(String[] args) {
+//         int a=10;
+//         for(int i=0;i<=a;i++){
+//             System.out.println("karthik");
+//         }
+        
+//     }
+// }
+// ---------------------------------
+// class Main{
+//     public static void main(String[] args) {
+//         int a=50;
+//         for(int i=1;i<=a;i++){
+//             if(i%2==0){
+//                 System.out.print(i+" ,");
+//             }
+//         }
+        
+//     }
+// }
+//-----------------------------------------------------------
+
+// import java.util.Scanner;
+
+// class Main{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the number:");
+//         int n = sc.nextInt();
+//         if(n==0||n==1){  
+//             System.out.println(n+" is not prime number");      
+//         }
+//         else{
+            // boolean isPrime = true;
+            // for (int i = 2; i < n; i++) {
+            //     if (n % i == 0) {
+            //         isPrime = false;
+            //         break;
+            //     }
+            // }
+            // if (isPrime) {
+            //     System.out.println(n + " is a prime number");
+            // } else {
+            //     System.out.println(n + " is not a prime number");
+            // }
+//         }
+//     }
+//     }
+// -------------------------------------------------------
+// class Main{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the number:");
+//         int n = sc.nextInt();
+//          if(n==0||n==1){  
+//         System.out.println(n+" is not prime number");      
+//     }
+//     else{
+//         for (int i = 1; i <= n; i++) {
+//             if (n % i == 0) {
+//                 System.out.print(i + " ");
+//             }
+//         }
+//     }
+//     }
+// }
+//=---------------------------------------------------------------------
+// class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the number:");
+//         int n = sc.nextInt();
+//         System.out.println("Prime numbers up to " + n + " are:");
+//         for (int i = 2; i <= n; i++) {
+//             if(isPrime(i)=true){
+//                 System.out.print(i + " ");
+//             }
+          
+//     }
+// }
+// static boolean isPrime(int n){
+    
+// }
+// }
+
+//----------------------------------------------------------------------------
